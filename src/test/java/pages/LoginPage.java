3fe4df95832925e93d9768c7eac99bd2dd668ab2
salekperova <s.alekperova1997@mail.ru>
2022-10-16 11:49:28 +0300

    12/10/22 ClassWork

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -11,6 +11,8 @@ public class LoginPage extends BasePage {
     private final By emailInputLocator = By.id("name");
     private final By pswInputLocator = By.id("password");
     private final By loginButtonLocator = By.id("button_primary");
+    private final By errorTextLocator = By.className("error-text");
+
 
 
     // Блок инициализации страницы
@@ -38,11 +40,8 @@ public class LoginPage extends BasePage {
     public void setEmail(String value){ getEmailInput().sendKeys(value);}
     public void setPsw(String value){ getPswInput().sendKeys(value);}
     public void clickLoginButton(){getLoginButton().click();}
+    public WebElement getErrorTextElement(){return driver.findElement(errorTextLocator);}
 
-    public void loginSuccessful(String email, String psw) {
-        setEmail(email);
-        setPsw(psw);
-        clickLoginButton();
-    }
+
 
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import BaseEntities.BaseTest;
 import configuration.ReadProperties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,20 +11,31 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
 import pages.LoginPage;
+import pages.projects.AddProjectPage;
+import pages.projects.UpdateProjectPage;
 import services.BrowsersService;
+import steps.LoginStep;
 
 public class LoginTest extends BaseTest {
 
     @Test
     public void successLoginTest() {
-        LoginPage loginPage = new LoginPage(driver);
+        loginStep.login(ReadProperties.username(), ReadProperties.password());
 
-        loginPage.loginSuccessful(ReadProperties.username(), ReadProperties.password());
         Assert.assertTrue(new DashboardPage(driver).isPageOpened());
     }
 
     @Test
-    public void incorrectUserNameTest() {
+    public void successLoginTest1() {
+        Assert.assertTrue(loginStep.loginSuccessful(ReadProperties.username(), ReadProperties.password()).
+                isPageOpened());
+    }
+
+    @Test
+    public void incorrectUsernameTest() {
+        Assert.assertEquals(loginStep.loginIncorrect("sdsd", ReadProperties.password()).getErrorTextElement()
+                .getText(), "Email/Login or Password is incorrect. Please try again.");
+
 //        WebElement username = driver.findElement(By.id("name"));
 //        WebElement psw = driver.findElement(By.id("password"));
 //        WebElement loginButton = driver.findElement(By.id("button_primary"));
@@ -31,11 +43,12 @@ public class LoginTest extends BaseTest {
 //        username.sendKeys(ReadProperties.username());
 //        psw.sendKeys(ReadProperties.password());
 //        loginButton.click();
-
-        DashboardPage dashboardPage = new DashboardPage(driver);
     }
 
     @Test
     public void incorrectPswTest() {
+        new AddProjectPage(driver).getSaveButton();
+        new UpdateProjectPage(driver).nameInput();
+
     }
 }
