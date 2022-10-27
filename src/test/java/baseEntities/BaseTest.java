3a33192d54a4ca22f26d09742e62ca29cfc3073a
salekperova <s.alekperova1997@mail.ru>
2022-10-27 18:13:47 +0300

    Allure CW

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -4,9 +4,11 @@ import configuration.ReadProperties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import services.BrowsersService;
 import steps.LoginStep;
 
+@Listeners({Listeners.class})
 public class BaseTest {
     protected WebDriver driver;
     protected LoginStep loginStep;
