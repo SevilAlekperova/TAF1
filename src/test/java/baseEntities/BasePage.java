f58550af57bc35e8b3ec8766867b76220a828f3d
salekperova <s.alekperova1997@mail.ru>
2022-11-13 17:03:18 +0300

    loadableComponent CW

--- src/test/java/baseEntities/BasePage.java
+++ src/test/java/baseEntities/BasePage.java
@@ -6,12 +6,26 @@ import org.openqa.selenium.WebDriver;
 import services.WaitsService;
 
 public abstract class BasePage {
+    protected static final int WAIT_FOR_PAGE_LOADED_IN_SECONDS = 30;
+
     protected WebDriver driver;
     protected WaitsService waitsService;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         waitsService = new WaitsService(driver);
+
+
+    }
+
+    public BasePage(WebDriver driver, boolean openPageByUrl) {
+        this.driver = driver;
+        waitsService = new WaitsService(driver);
+
+        if (openPageByUrl) {
+            openPage();
+        }
+        waitForOpen();
     }
 
     protected abstract By getPageIdentifier();
@@ -20,7 +34,24 @@ public abstract class BasePage {
         return driver.findElement(getPageIdentifier()).isDisplayed();
     }
 
-    public void openPageByUrl(String pagePath) {
-        driver.get(ReadProperties.getUrl() + pagePath);
+
+    public abstract void openPage();
+
+    protected void waitForOpen() throws InterruptedException {
+        int tryCount = 0;
+        boolean isPageOpenedIndicator = isPageOpened();
+
+        while (!isPageOpenedIndicator
+                && tryCount < (WAIT_FOR_PAGE_LOADED_IN_SECONDS)) {
+            tryCount++;
+            Thread.sleep(1000);
+            isPageOpenedIndicator = isPageOpened();
+        }
+
+        if (!isPageOpenedIndicator) {
+            throw new AssertionError("Page was not opened");
+        }
     }
 }
+
+
